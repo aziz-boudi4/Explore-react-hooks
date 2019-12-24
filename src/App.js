12dc1d755f45c33f9921384e8fac2ab19c3c8148
Aziz Boudi <azicode4@gmail.com>
2019-12-24 13:23:59 +0100

    extract the counter

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Counter from './Components/Counter'
 
 const App = () => {
   const [count, setCount] = useState(0)
 
-  const add = () => {
-    setCount(count + 1)
-  }
-
-  const substract = () => {
-    if (count > 0) {
-      setCount(count - 1)
-    }
-  }
-
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Counter with Hooks using useState </h2>
-        counter result : {count}
-        <div>
-          <button className="counterBtn" onClick={substract}>-</button>
-          <button className="counterBtn" onClick={add}>+</button>
-        </div>
+        <Counter count={count} setCount={setCount}/>
         <hr width="600"/>
       </header>
     </div>
---
+++ src/Components/Counter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Counter = ({count,setCount}) => {
+
+  const add = () => {
+    setCount(count + 1)
+  }
+
+  const substract = () => {
+    if (count > 0) {
+      setCount(count - 1)
+    }
+  }
+
+  return (
+    <div>
+      <h2>Counter with Hooks using useState </h2>
+      counter result : {count}
+      <div>
+        <button className="counterBtn" onClick={substract}>-</button>
+        <button className="counterBtn" onClick={add}>+</button>
+      </div>
+    </div>
+
+  );
+}
+
+
+
+
+export default Counter;
