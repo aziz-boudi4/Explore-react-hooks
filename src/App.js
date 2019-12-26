4201877ed34f3bbc91a567afe91dd164a9a438dd
Aziz Boudi <azicode4@gmail.com>
2019-12-26 14:41:56 +0100

    forgotten files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Counter from './Components/Counter'
 
 const App = () => {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
 
   return (
     <div className="App">
